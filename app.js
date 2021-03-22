87207f93d8a89d64f83ef6ce473eb23c9b260fa1
Jackson Pham <jackson.pham@gmail.com>
2021-03-22 13:34:36 -0700

    Completed Making List Bold When Hover

--- app.js
+++ app.js
@@ -7,47 +7,51 @@ var App = () => (
 
 var TodoList = () => (
   <div>
-      <GroceryList/>
+      <GroceryList groceryListItem={['Ham','Cheese','Honey']}/>
   </div>
 
 );
 
-var GroceryList = () => (
-  <div>
-    <GroceryListItem groceryListItem={['Ham','Cheese','Honey']}/>
-  </div>
- )
+var GroceryList = (props) => (
+  <ul>
+    {props.groceryListItem.map(item =>
+      <GroceryListItem item={item}/>
+    )}
+  </ul>
+ );
 
-
-// var GroceryListItem = (props) => {
-
-//   var onListItemClick = (event) => {
-//     console.log('I got clicked!');
-//   }
-
-//   return (
-//     <ul>
-//       {/* {console.log('this is the props', props)} */}
-//       <li onClick={onListItemClick}>{props.groceryListItem[0]}</li>
-//       <li>{props.groceryListItem[1]}</li>
-//       <li>{props.groceryListItem[2]}</li>
-//     </ul>
-//   )
-// }
 class GroceryListItem extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      hover: false
+    }
   }
 
+  toggleHover() {
+    this.setState({
+      hover: !this.state.done
+    });
+  }
+
+
   render() {
+
+    // var style = {
+    //   textDecoration: this.state.done ? 'line-through' : 'none'
+    // };
+
+    var style;
+    if (this.state.hover) {
+      style = {cursor: 'pointer', fontWeight: 'bolder'}
+    } else {
+      style = {cursor: 'pointer'}
+    }
+
     return (
-      <ul>
-        {/* {console.log('this is the props', props)} */}
-        <li>{this.props.groceryListItem[0]}</li>
-        <li>{this.props.groceryListItem[1]}</li>
-        <li>{this.props.groceryListItem[2]}</li>
-      </ul>
+      <li style={style}  onMouseOver={this.toggleHover.bind(this)}>{this.props.item}</li>
     )
   }
 }
